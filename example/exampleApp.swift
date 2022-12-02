2ab890afbf84fa4f7c70414f7ed24a8302ec4c84
JIN-JJS <joonsu2005@naver.com>
2022-12-02 10:19:34 +0900

    Hello

---
+++ example/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by 전준수 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
